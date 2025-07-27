2039048a356cccb8b3b1490c8713f9c2541cc713
Davibesxxol <davi19silva00@gmail.com>
2025-07-26 22:19:51 -0300

    Questões

---
+++ Cálculos.py
@@ -0,0 +1,26 @@
+print("  *****  ")
+print("(* o o *)")
+print(" *  >  * ")
+print(" * ___ *")
+print("  ***** ")
+
+amigos = 20
+cada = 35
+print('Valor da conta: R$', amigos * cada)
+
+amigos = 4
+conta = 180
+print('Cada um deve pagar:R$', (conta / amigos))
+
+conta = 90
+heraclito = 55
+print('Fredegunda deve pagar R$', conta - heraclito)
+
+presente = 150
+ana = 37.5
+joana = 2* ana
+carlos = ana
+print('ana deve pagar R$', ana)
+print('joana deve pagar', joana)
+print('carlos deve pagar', carlos)
+
